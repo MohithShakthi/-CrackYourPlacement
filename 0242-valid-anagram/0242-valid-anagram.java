286c32ab9049580d33ef5f37d7e01420cbd73b09
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-13 12:29:55 +0530

    Time: 3 ms (90.2%), Space: 44.7 MB (33.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,12 +5,15 @@ class Solution {
         }
         char[] charS = s.toCharArray();
         char[] charT = t.toCharArray();
-        Map<Character,Integer> mapS = new HashMap<>();
+        /*Map<Character,Integer> mapS = new HashMap<>();
         Map<Character,Integer> mapT = new HashMap<>();
         for(int i=0; i<charS.length; i++){
             mapS.put(charS[i], mapS.getOrDefault(charS[i],0)+1);
             mapT.put(charT[i], mapT.getOrDefault(charT[i],0)+1);
         }
-        return mapS.equals(mapT);
+        return mapS.equals(mapT);*/
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+        return Arrays.equals(charS, charT);
     }
 }

eddc6de7b9057cf6b2df455374e373e4a286a486
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-20 19:04:45 +0530

    Time: 2 ms (89.25%), Space: 55.9 MB (31.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-     int[] left = new int[nums.length];
-     int[] right = new int[nums.length];
-     left[0] =1;
-     for(int i=1;i<nums.length;i++){
-        left[i] = left[i-1]*nums[i-1];
-     }
-     right[nums.length-1]=1;
-     for(int i=nums.length-2;i>-1;i--){
-        right[i] = right[i+1]*nums[i+1];
-     }
-     int[] ans = new int[nums.length];
-     for(int i=0;i<nums.length;i++){
-        ans[i] = left[i]*right[i];
-     }    
-     return ans;
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        prefix[0] = 1;
+        suffix[nums.length-1] = 1;
+        int sum = 1;
+        for(int i=1;i<nums.length;i++){
+            sum = sum*nums[i-1];
+            prefix[i] = sum;
+        }
+        sum =1;
+        for(int i=nums.length-2;i>=0;i--){
+            sum = sum*nums[i+1];
+            suffix[i] = sum;
+        }
+        for(int i=0;i<nums.length;i++) nums[i] = prefix[i]*suffix[i];
+        return nums;
     }
 }

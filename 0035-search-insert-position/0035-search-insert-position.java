13fdcf30c04aba550a6f8d3510c8388ca5652371
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-23 19:26:44 +0530

    Time: 0 ms (100%), Space: 43.1 MB (29.04%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,7 +2,7 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int[] res = binary(nums,target);
         if(res[0] != -1) return res[0];
-        return res[1]+1;
+        return res[1];
     }
     public int[] binary(int[] arr, int k){
         int s=0;
@@ -15,6 +15,6 @@ class Solution {
             else if(k > arr[mid]) s= mid+1;
             else e= mid-1;
         }
-        return new int[]{-1,e};
+        return new int[]{-1,s};
     }
 }

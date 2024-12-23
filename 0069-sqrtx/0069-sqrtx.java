d2c4fc8e30fcace053a50e9eeda5663e7ba0dc4d
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-23 19:46:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,11 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        long n = 1;
-        long r = n * n;
-        while(x >= r){
-            n++;
-            r = n * n;
+        int s  = 1;
+        int e = x;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid > x) e = mid-1;
+            else s = mid+1;
         }
-        return (int)n-1;
+        return e;
     }
 }

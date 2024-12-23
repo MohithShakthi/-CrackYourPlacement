b7639c6b974fd8162506e953f7447fe0f2e07d2a
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-23 11:48:04 +0530

    Time: 1 ms (25.8%), Space: 50.2 MB (67.04%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 1) return nums[0];
         return nums[binary(nums)];
     }
     public int binary(int[] nums){
@@ -8,9 +9,9 @@ class Solution {
         while(s<=e){
             int mid = s+(e-s)/2;
             if(mid > 0 && mid < nums.length-2 && nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) return mid;
-            else if(s == 0 && nums.length > 1 && nums[s] < nums[s+1]) return s;
+            else if(s == 0 && nums[s] < nums[s+1]) return s;
             else if(s > 0 && nums[s] > nums[s-1] && nums[s] < nums[s+1]) return s;
-            else if(e == nums.length-1 && nums.length > 1 && nums[e-1] < nums[e]) return e;
+            else if(e == nums.length-1 && nums[e-1] < nums[e]) return e;
             else if(e < nums.length-1 && nums[e] > nums[e-1] && nums[e] < nums[e+1]) return e;
             else{
                 s++;

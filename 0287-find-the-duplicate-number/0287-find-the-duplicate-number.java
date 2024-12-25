750d99cd1489cc921afae860c112e7f8326dc210
Mohith Shakthi T <146347506+MohithShakthi@users.noreply.github.com>
2024-12-25 19:57:32 +0530

    Time: 8 ms (38.67%), Space: 58.6 MB (25.55%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,26 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        cycleSort(nums);
+        int count = 0;
         for(int i=0;i<nums.length;i++){
-            if(i>0 && set.contains(nums[i])){
-                return nums[i];
-            }
-            set.add(nums[i]);
+            if(nums[i] != i+1) return nums[i];
         }
-        return -1;
+        return count;
+    }
+    public void cycleSort(int[] nums){
+        int i=0;
+        while(i<nums.length){
+            int correctIndex = nums[i]-1;
+            if(nums[i] != nums[correctIndex]){
+                swap(i,correctIndex,nums);
+            }else{
+                i++;
+            }
+        }
+    }
+    public void swap(int a, int b, int[] nums){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp; 
     }
 }
